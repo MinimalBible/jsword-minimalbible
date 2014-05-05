4f03086a36c82f84e1258880b2a574ca566b5094
Bradlee Speice <bspeice.nc@gmail.com>
2014-05-05 00:53:32 -0400

    Tweaks to the jsword conditional build Also add a FilterUtil class to JSword

---
+++ src/main/java/jsword/org/crosswire/jsword/book/FilterUtil.java
@@ -0,0 +1,51 @@
+package org.crosswire.jsword.book;
+
+import org.crosswire.jsword.book.BookCategory;
+import org.crosswire.jsword.book.BookFilter;
+import org.crosswire.jsword.book.BookFilters;
+
+import java.util.Arrays;
+
+/**
+ * Module to help with some conversions between BookCategories and BookFilters
+ * Makes more sense to move it to JSword than incorporate in MinimalBible
+ */
+public class FilterUtil {
+
+    private static final BookCategory[] MAPPABLE_CATEGORIES = {BookCategory.BIBLE,
+            BookCategory.COMMENTARY, BookCategory.DAILY_DEVOTIONS, BookCategory.DICTIONARY,
+            BookCategory.GENERAL_BOOK, BookCategory.GLOSSARY, BookCategory.MAPS,
+            BookCategory.OTHER
+    };
+    private static final BookFilter[] MAPPABLE_FILTERS = {BookFilters.getBibles(),
+            BookFilters.getCommentaries(), BookFilters.getDailyDevotionals(), BookFilters.getDictionaries(),
+            BookFilters.getGeneralBooks(), BookFilters.getGlossaries(), BookFilters.getMaps(),
+            BookFilters.getNonBibles()
+    };
+
+    public static BookCategory categoryFromFilter(BookFilter f)
+            throws InvalidFilterCategoryMappingException {
+        int index = Arrays.asList(MAPPABLE_FILTERS).indexOf(f);
+        if (index != -1) {
+            return MAPPABLE_CATEGORIES[index];
+        } else {
+            throw new InvalidFilterCategoryMappingException("Can not map from filter: " + f.toString() + " to category.");
+        }
+    }
+
+    public static BookFilter filterFromCategory(BookCategory c)
+            throws InvalidFilterCategoryMappingException {
+        int index = Arrays.asList(MAPPABLE_CATEGORIES).indexOf(c);
+        if (index != -1) {
+            return MAPPABLE_FILTERS[index];
+        } else {
+            throw new InvalidFilterCategoryMappingException("Can not map from category: " + c.toString() + " to filter.");
+        }
+    }
+
+    public static class InvalidFilterCategoryMappingException extends Exception {
+        public InvalidFilterCategoryMappingException(String message) {
+            super(message);
+        }
+    }
+}
